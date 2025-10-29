27da432343a10f2c354c6dd261199de3fe173da4
Matheus-Leffa <matheusleffa@live.com>
2025-10-29 19:04:03 -0300

    Implementei classe calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,13 @@
 public class Calculadora {
+    private HistoricoCalculos historico = new HistoricoCalculos();
+
+    public double calcular(Operacao calculo){
+        calculo.calcular();
+        historico.adicionar(calculo);
+        return calculo.getResult();
+    }
+
+    public void mostrarHistorico(){
+        historico.mostrarHistorico();
+    }
 }
